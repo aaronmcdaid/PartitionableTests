6a10b30b2a5f081bb7ae65453e44673db0606b6b
Aaron McDaid <aaronmcdaid@gmail.com>
2011-02-18 19:32:37 +0000

    count the components

--- code/NSC.cpp
+++ code/NSC.cpp
@@ -33,12 +33,75 @@ struct RandomCommunityAssignment {
 	}
 };
 
+struct Coloring {
+	const RandomCommunityAssignment * const rca;
+	const int N;
+	vector<int> colors;
+	set<int> uncoloredCommunities;
+	int componentsSoFar;
+	Coloring(const RandomCommunityAssignment *_rca): rca(_rca), N(_rca->N), colors(N, -1), componentsSoFar(0) {
+		for(int c=0; c<this->rca->C; c++)
+			uncoloredCommunities.insert(c);
+		assert(uncoloredCommunities.size() == this->rca->C);
+	}
+	void colorOneComm(const int commID, const int color) {
+		const bool wasErased = uncoloredCommunities.erase(commID);
+		assert(wasErased);
+		const set<int> members = this->rca->communities.at(commID);
+		forEach(const int n, amd::mk_range(members)) {
+			if(this->colors.at(n) != color) {
+				assert(this->colors.at(n) == -1);
+				this->colors.at(n) = color;
+			}
+		}
+	}
+	int fixMiscolorings() {
+		forEach(const int maybeColored, amd::mk_range(this->uncoloredCommunities)) { // the ID of a community that might just have been percolated into
+			forEach(const int n, amd::mk_range(this->rca->communities.at(maybeColored))) { // the nodes in that community
+				const int color = this->colors.at(n);
+				if(color == -1) { // that's OK, this node is still, as expected, uncolored
+				} else {
+					// AHA. We must color this community and restart
+					PP3(maybeColored, n, color);
+					this->colorOneComm(maybeColored, color);
+					return 1+this->fixMiscolorings();
+				}
+			}
+		}
+		return 0;
+	}
+	int percolateAnotherComponent() {
+		const int newColor = componentsSoFar;
+		// 1. take next uncoloured component
+		// 2. color its nodes in
+		// 3. find a miscoloured community.
+		//   3a. if found, colors its nodes in and repeat
+    // end
+		if(uncoloredCommunities.empty())
+			return -1; // all done!
+		const int seedComm = *uncoloredCommunities.begin();
+		PP2(seedComm, newColor);
+
+		this->colorOneComm(seedComm, newColor);
+		const int newlyColored = 1+this->fixMiscolorings();
+		PP(newlyColored);
+
+		++componentsSoFar;
+		return newColor;
+	}
+};
+
 void doExperiments(const int N, const int S, const int C) {
 	const RandomCommunityAssignment rca(N,S,C);
 	PP(rca.commsPerNodeAvg());
 	PP(rca.numOrphanNodes());
 	cout << endl;
 
+	Coloring col(&rca);
+	while(!col.uncoloredCommunities.empty()) {
+		col.percolateAnotherComponent();
+	}
+	PP(col.componentsSoFar);
 }
 
 struct UsageMessage{};
